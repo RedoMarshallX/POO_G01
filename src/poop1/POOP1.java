287d24cf951e83dc268b272fc8b2ac50ab5e5f8b
Issa <issa35rom31@gmail.com>
2022-09-02 01:35:29 -0500

    Primera practica

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,13 @@
+
+package poop1;
+
+/**
+ *
+ * @author Isaías Rosas Meza
+ */
+public class POOP1 {
+    public static void main(String[] args) {
+        System.out.println("¡Hola mundo!");
+    }
+    
+}
